e5f50ffb2e8c40e3ee2c9fbadac9cfcca88660f6
Andrew <pimanac@users.noreply.github.com>
2017-01-23 23:53:40 -0500

    Add missing ModAction properties.

--- RedditSharp/Things/ModAction.cs
+++ RedditSharp/Things/ModAction.cs
@@ -25,10 +25,22 @@ namespace RedditSharp.Things
         public DateTime? TimeStamp { get; set; }
 
         /// <summary>
+        /// Populated when <see cref="Action"/> is WikiBan, BanUser, or UnBanUser.
+        /// </summary>
+        [JsonProperty("description")]
+        public string Description { get; set; }
+
+        /// <summary>
         /// Action details.
         /// </summary>
         [JsonProperty("details")]
         public string Details { get; set; }
+
+        /// <summary>
+        /// Base 36 id of the moderator who performed the action.
+        /// </summary>
+        [JsonProperty("mod_id36")]
+        public string ModeratorId { get; set; }
 
         /// <summary>
         /// Name of moderator who performed the action.
@@ -53,6 +65,30 @@ namespace RedditSharp.Things
         /// </summary>
         [JsonProperty("target_permalink")]
         public string TargetThingPermalink { get; set; }
+
+        /// <summary>
+        /// Base 36 id of the subreddit.
+        /// </summary>
+        [JsonProperty("sr_id36")]
+        public string SubredditId { get; set; }
+
+        /// <summary>
+        /// Subreddit name.
+        /// </summary>
+        [JsonProperty("subreddit")]
+        public string SubredditName { get; set; }
+
+        /// <summary>
+        /// Populated when target is a comment.
+        /// </summary>
+        [JsonProperty("target_body")]
+        public string TargetBody { get; set; }
+
+        /// <summary>
+        /// Populated when target is a post.
+        /// </summary>
+        [JsonProperty("target_title")]
+        public string TargetTitle { get; set; }
 
         /// <summary>
         /// Author of the item against which this moderation action was performed.

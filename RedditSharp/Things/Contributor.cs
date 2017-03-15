8578f68a354d94f5612d74ec88ad4c875c5a8c68
Andrew <pimanac@users.noreply.github.com>
2017-03-14 21:34:56 -0400

    Contributor should also inherit from RedditUser.

--- RedditSharp/Things/Contributor.cs
+++ RedditSharp/Things/Contributor.cs
@@ -1,16 +1,26 @@
 ﻿using System;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
+using RedditSharp.Extensions;
 
 namespace RedditSharp.Things
 {
     /// <summary>
     /// A contributor to a subreddit.
     /// </summary>
-    public class Contributor : Thing
+    public class Contributor : RedditUser
     {
         #pragma warning disable 1591
         public Contributor(Reddit reddit, JToken json) : base(reddit, json) {
+            var data = json["name"] == null ? json["data"] : json;
+            base.Name = data["name"].ValueOrDefault<string>();
+            var id = data["id"].ValueOrDefault<string>();
+            if (id.Contains("_"))
+            {
+                base.Kind = "t2";
+                base.Id = id.Split('_')[1];
+                base.FullName = id;
+            }
         }
         #pragma warning restore 1591
 
@@ -26,5 +36,16 @@ namespace RedditSharp.Things
         [JsonProperty("date")]
         [JsonConverter(typeof(UnixTimestampConverter))]
         public DateTime DateAdded { get; private set; }
+
+        /// This will always return 0 for Contributors
+        /// </summary>
+        [JsonIgnore]
+        public new int CommentKarma => 0;
+
+        /// <summary>
+        /// This will always return 0 for Contributors
+        /// </summary>
+        [JsonIgnore]
+        public new int LinkKarma => 0;
     }
 }

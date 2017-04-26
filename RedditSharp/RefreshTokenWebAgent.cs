1254f77ebd9c8504dac1fba6fa8c83f764d6b22e
CrustyJew <mattiprice520@gmail.com>
2017-04-25 22:54:41 -0500

    More work on web agent pool.
    Changing to specific implementation for refresh tokens + new web agent.

---
+++ RedditSharp/RefreshTokenWebAgent.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace RedditSharp
+{
+    /// <summary>
+    /// WebAgent supporting OAuth.
+    /// </summary>
+    public class RefreshTokenWebAgent : WebAgent
+    {
+        /// <summary>
+        /// Minutes before token expiration to get a new token. Defaults to 5
+        /// </summary>
+        public int RenewTokenThreshold { get; set; }
+        //private so it doesn't leak app secret to other code
+        private AuthProvider TokenProvider;
+        private string RefreshToken;
+
+        /// <summary>
+        /// DateTime the token expires.
+        /// </summary>
+        public DateTime TokenValidTo { get; set; }
+
+        /// <summary>
+        /// A web agent using reddit's OAuth interface.
+        /// </summary>
+        /// <param name="username">The username.</param>
+        /// <param name="password">The user's password.</param>
+        /// <param name="clientID">Granted by reddit as part of app.</param>
+        /// <param name="clientSecret">Granted by reddit as part of app.</param>
+        /// <param name="redirectURI">Selected as part of app. Reddit will send users back here.</param>
+        public RefreshTokenWebAgent(string refreshToken, string clientID, string clientSecret, string redirectURI, string accessToken = "", DateTime? validTo = null, RateLimitManager rateLimiter = null):base(accessToken,rateLimiter)
+        {
+            RefreshToken = refreshToken;
+            AccessToken = accessToken;
+            TokenValidTo = validTo.HasValue && !string.IsNullOrWhiteSpace(accessToken) ? validTo.Value : DateTime.UtcNow.AddMinutes(-1);
+            RootDomain = "oauth.reddit.com";
+            RenewTokenThreshold = 5;
+            TokenProvider = new AuthProvider(clientID, clientSecret, redirectURI, this);
+        }
+
+        /// <inheritdoc/>
+        public override HttpRequestMessage CreateRequest(string url, string method)
+        {
+            if (url != AuthProvider.AccessUrl && DateTime.UtcNow.AddMinutes(RenewTokenThreshold) > TokenValidTo)
+            {
+                Task.Run(GetNewTokenAsync).Wait();
+            }
+            return base.CreateRequest(url, method);
+        }
+
+        /// <inheritdoc/>
+        protected override HttpRequestMessage CreateRequest(Uri uri, string method)
+        {
+            if (uri.ToString() != AuthProvider.AccessUrl && DateTime.UtcNow.AddMinutes(RenewTokenThreshold) > TokenValidTo)
+            {
+                Task.Run(GetNewTokenAsync).Wait();
+            }
+            return base.CreateRequest(uri, method);
+        }
+
+        private async Task GetNewTokenAsync()
+        {
+            AccessToken = await TokenProvider.GetOAuthTokenAsync(RefreshToken,true).ConfigureAwait(false);
+            TokenValidTo = DateTime.UtcNow.AddHours(1);
+        }
+    }
+
+}
---
+++ RedditSharp/RefreshTokenWebAgentPool.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.Extensions.Caching.Memory;
+using System.Linq;
+
+namespace RedditSharp
+{
+    public class RefreshTokenWebAgentPool
+    {
+        public int SlidingExpirationMinutes { get; set; }
+        public string DefaultUserAgent { get; set; }
+        public RateLimitMode DefaultRateLimitMode { get; set; }
+
+        private List<UserWebAgent> userWebAgents = new List<UserWebAgent>();
+        private MemoryCache activeAgentsCache = new MemoryCache(new MemoryCacheOptions() { CompactOnMemoryPressure = true });
+
+        private string ClientID;
+        private string ClientSecret;
+        private string RedirectURI;
+
+        public RefreshTokenWebAgentPool(string clientID, string clientSecret, string redirectURI)
+        {
+            ClientID = clientID;
+            ClientSecret = clientSecret;
+            RedirectURI = redirectURI;
+        }
+
+
+        public IWebAgent GetWebAgent(string username, string accessToken = "", DateTime? accessTokenExpires = null, string refreshToken = "")
+        {
+            if (string.IsNullOrWhiteSpace(username)) throw new ArgumentException("username cannot be null or empty");
+
+            var uwa = userWebAgents.SingleOrDefault(a => a.Username.ToLower() == username.ToLower());
+            
+            if(uwa == null)
+            {
+                if(!string.IsNullOrWhiteSpace(refreshToken))
+                {
+                    uwa = new UserWebAgent() {
+                        Username = username,
+                        AccessToken = accessToken,
+                        TokenExpires = accessTokenExpires ?? DateTime.UtcNow.AddMinutes(45),
+                        RefreshToken = refreshToken,
+                        WebAgentID = new Guid()
+                    };
+
+                    userWebAgents.Add(uwa);
+                    var agent = new RefreshTokenWebAgent(refreshToken, ClientID, ClientSecret, RedirectURI, uwa.AccessToken, uwa.TokenExpires, new RateLimitManager(DefaultRateLimitMode));
+                    activeAgentsCache.Set(uwa.WebAgentID, agent );
+                    return agent;
+                }
+                return null;
+            }
+            activeAgentsCache;
+        }
+
+    }
+
+    internal class UserWebAgent
+    {
+        public string Username { get; set; }
+        public string AccessToken { get; set; }
+        public string RefreshToken { get; set; }
+        public DateTime TokenExpires { get; set; }
+
+        public Guid WebAgentID { get; set; }
+
+
+    }
+}

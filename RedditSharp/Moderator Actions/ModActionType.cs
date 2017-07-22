4f4a22247eb511d43217d117735e01a9295a6ecd
zetec <veruszetec@gmail.com>
2017-07-21 22:56:31 -0500

    Added Spoiler and UnSpoiler modAction enums

--- RedditSharp/Moderator Actions/ModActionType.cs
+++ RedditSharp/Moderator Actions/ModActionType.cs
@@ -35,6 +35,8 @@ namespace RedditSharp
         SetSuggestedsort,
         Sticky,
         SpamComment,
+        Spoiler,
+        UnSpoiler,
         UnSticky,
         SetContestMode,
         UnSetContestMode,

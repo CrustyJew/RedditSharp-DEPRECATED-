5ba0100193846f48fbf99df21feef6d5fdf9a942
Chris F <fingarc@gmail.com>
2017-12-26 12:46:56 -0800

    Initial work toward allowing compound searches
    Allowing single property comparison so far.  Holding this code to try a
    different approach for AND search operator.

---
+++ RedditSharp/Search/ISearchFormatter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace RedditSharp.Search
+{
+    public interface ISearchFormatter
+    {
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="search"></param>
+        /// <returns></returns>
+        /// <remarks>
+        /// has to be Expression Func https://michael-mckenna.com/func-t-vs-expression-func-t-in-linq/
+        /// </remarks>
+        string Format(Expression<Func<AdvancedSearchFilter, bool>> search);
+    }
+}

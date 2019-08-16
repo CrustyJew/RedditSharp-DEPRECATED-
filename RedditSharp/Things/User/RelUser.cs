251deb0bd7850527f3b8fd7a57b3fbe6333c1d7d
Justin Krejcha <justin@justinkrejcha.com>
2019-08-15 20:45:32 -0700

    Implement PartialUser and RelUser.

---
+++ RedditSharp/Things/User/RelUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace RedditSharp.Things.User
+{
+	public class RelUser : PartialUser
+	{
+		public RelUser(IWebAgent agent, JToken json) : base(agent, json)
+		{
+		}
+
+		/// <summary>
+		/// UTC time of when this user was added to the list
+		/// </summary>
+		[JsonProperty("date")]
+		[JsonConverter(typeof(UnixTimestampConverter))]
+		public DateTime? DateUTC { get; set; }
+	}
+}

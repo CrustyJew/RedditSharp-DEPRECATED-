2e1fd103859072b56d7ecf90a52a81c5a860f5bd
CSedar21 <csedar21@gmail.com>
2019-11-11 16:52:19 -0800

    Adding the preview property from the Reddit API post response to the post class (#216)
    * Adding the preview property from the Reddit API post response to the post class
    
    * Changing Image class Width and Height properties from int to int?

---
+++ RedditSharp/Things/Preview.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RedditSharp.Things
+{
+    /// <summary>
+    /// Post preview images.
+    /// </summary>
+    public class Preview
+    {
+        public Preview(IWebAgent agent, JToken json)
+        {
+        }
+
+        /// <summary>
+        /// List of post image in various resolutions.
+        /// </summary>
+        [JsonProperty("images")]
+        public List<Images> Images { get; private set; }
+
+        /// <summary>
+        /// Is there a preview enabled for this post.
+        /// </summary>
+        [JsonProperty("enabled")]
+        public bool Enabled { get; private set; }
+    }
+}
